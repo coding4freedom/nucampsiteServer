dcf156a9955f7fbb41e410fe9b7c7475ab2a834c
Chris <chawkins2010@hotmail.com>
2021-02-20 15:27:19 -0600

    Passport Facebook

--- models/campsite.js
+++ models/campsite.js
@@ -16,7 +16,7 @@ const commentSchema = new Schema({
         required: true
     },
     author: {
-        type: mongoose.Schema.Types.ObjectId,
+        type: Schema.Types.ObjectId,
         ref: 'User'
     }
 }, {
